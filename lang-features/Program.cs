747f8221a8771de163a01233d9bfbc1147a16e5a
yaroslavx <yakupov.yaroslav@icloud.com>
2025-03-21 21:10:04 +0300

    MAINTAIN: Made some testing

--- lang-features/Program.cs
+++ lang-features/Program.cs
@@ -6,3 +6,15 @@ VariablesScopes.Main();
 Overflow.Main();
 IncDec.Main();
 SomeMathOperations.Main();
+
+string name = "randomName";
+string name1 = new string("randomName1");
+
+bool isContains = name.Contains("randomName");
+
+string abs = string.Concat(name, name1);
+
+int x = 1;
+string strX = x.ToString();
+
+Console.WriteLine(strX);
